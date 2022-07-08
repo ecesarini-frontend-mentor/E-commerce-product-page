2299aa37321b3b5c649fed9a284f0e8477452aff
ecesarini <73652902+ecesarini@users.noreply.github.com>
2022-07-08 23:58:03 +0200

    check ecpp.js

--- js/gal.js
+++ js/gal.js
@@ -4,39 +4,74 @@ export class Gal extends GalTools {
     constructor() {
         super();
         this.mainContainer = document.querySelector('main');
-        this.modalClk = document.querySelector('.product-sel');
+        this.modalOnClk = document.querySelector('.product-sel');
         this.tmbClk = document.querySelectorAll('#image-side .tmb-btn');
+        this.modalBtnClk = null;
         this.eventsListener();
     }
     eventsListener() {
-        this.modalClk.addEventListener('click', this);
+        this.modalOnClk.addEventListener('click', this);
         this.tmbClk.forEach(t => t.addEventListener('click', this));
+        if(this.modalBtnClk) {
+            this.modalBtnClk.forEach(m => m.addEventListener('click', this));
+        }
     }
     handleEvent(e) {
         const ect = e.currentTarget;
 
         switch(e.type) {
             case 'click':
+                if(ect.matches('.tmb-btn')) {
+                    this.thumbSel(ect);
+                }
                 if(ect.matches('.product-sel')) {
                     this.modalOn();
                 }
-                if(ect.matches('.tmb-btn')) {
-                    this.thumbSel(ect);
+                if(ect.matches('.modal-close-button')) {
+                    this.modalOff();    
+                }
+                if(ect.matches('.modal-top-btn')) {
+                    this.modalSliderTopBtn(ect);
                 }
                 break;
         }
     }
-    modalOn() {
-        let modal = this.modalCreator(this.modalClk.querySelector('img'));
-        this.mainContainer.append(modal);
-        modal.style.display = 'block';
-
-        //debugger;
-    }
-
+    
     thumbSel(ect) {
         let imgMain = document.querySelector('.product-sel img'),
             newImgSrc = ect.querySelector('img').src.replace('-thumbnail', '');
         imgMain.src = newImgSrc;
+    
+    }
+    modalSliderTopBtn(ect) {
+        /*const mainImg = (ect) => {
+            const ris = null;
+            if (ect.matches('.modal-top-btn-previous')) {
+                debugger;
+            }
+            else if(ect.matches('.modal-top-btn-next')) {
+                debugger;
+            }
+        }*/
+        if (ect.matches('.modal-top-btn-previous')) {
+            debugger;
+        }
+        else if(ect.matches('.modal-top-btn-next')) {
+            debugger;
+        }
+        
+    }
+    modalOn() {
+        let modal = this.modalCreator(this.modalOnClk.querySelector('img'));
+        this.mainContainer.append(modal);
+        modal.style.display = 'block';
+        this.modalBtnClk = document.querySelectorAll('.modal-container button');
+        this.eventsListener();
+    }
+    modalOff() {
+        //modal.style.display = 'none';
+        document.querySelector('.modal').remove();
+        this.modalBtnClk = null;
+        this.eventsListener();
     }
 }
