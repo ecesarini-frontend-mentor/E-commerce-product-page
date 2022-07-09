b33ed6717fc9333419b85b376906dfe10ae235d7
ecesarini <73652902+ecesarini@users.noreply.github.com>
2022-07-10 00:50:21 +0200

    ics

--- js/ecpp.js
+++ js/ecpp.js
@@ -1,16 +1,14 @@
 import {Gal} from './gal.js';
 
-// TODO:
-//  'gal.js.modalSliderTopBnt':
-//  catch the img inside slideshow
+/*  FIXED: Modal container interactive; @gal.js:32 @style.css:235
+        -'gal.js.modalSliderTopBnt':
+        - catch the img inside slideshow
 
-// FIXED: Modal container interactive; @gal.js:32 @style.css:235
+    TODO:
+        - filter modal container click event; @style.css:300
+*/
 
 function ecppjs() {
-    
-        //debug modal
-        let mdl = document.querySelectorAll('.modal');
-        if(mdl) mdl.forEach(m => m.remove());
     new Gal();
 }
 
