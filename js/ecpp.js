8d3d95b815d38d66446708e44b36ae01ff4bcf62
ecesarini <73652902+ecesarini@users.noreply.github.com>
2022-07-10 23:59:09 +0200

    check ecpp.js TODO

--- js/ecpp.js
+++ js/ecpp.js
@@ -1,11 +1,7 @@
 import {Gal} from './gal.js';
-
-/*  FIXED: Modal container interactive; @gal.js:32 @style.css:235
-        -'gal.js.modalSliderTopBnt':
-        - catch the img inside slideshow
-
+/*
     TODO:
-        - filter modal container click event; @style.css:300
+        - populate cart selection;
 */
 
 function ecppjs() {
