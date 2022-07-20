1d3a33b154549afe41504bb0cb854ff526aad96f
ecesarini <73652902+ecesarini@users.noreply.github.com>
2022-07-21 00:24:27 +0200

    todo @ ecpp.js

--- js/ecpp.js
+++ js/ecpp.js
@@ -5,10 +5,11 @@ import {Gal} from './gal.js';
         - start from 'gal.js@203' and check 'main' background in 'style.css'. It doesn't show style pattern requested;
         - some fixes about style;
         - 'gal.js@83' about changing on viewport. Fix switching from mobile to desktop.
-    
+        - check: Desktop:\\gal-style.check.jpg: '.pop-cart-create' ./style.css@353 positioning; ./js/galmq.js@21,22 behavior. Dependend on parent containers' height.
     TODO: 
-        - check: Desktop:\\gal-style.check.jpg: '.pop-cart-create' ./style.css@353 positioning; ./js/galmq.js@21,22 behavior.
-
+        - transitioning leftbar: (ref: https://stackoverflow.com/questions/3331353/transitions-on-the-css-display-property)
+            './style.css@490' 'index.html@22' 'js/gal.js@76'
+        - check style.css@582:'overflow-y' to accomplish design on 375px instead 700px :)
 */
 
 function ecppjs() {
