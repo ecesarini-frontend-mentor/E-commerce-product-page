fc1449e2a8a2228c88838587c576f07539c19e40
ecesarini <73652902+ecesarini@users.noreply.github.com>
2022-07-22 00:27:08 +0200

    check jecpp.js TODO for slidebar

--- js/ecpp.js
+++ js/ecpp.js
@@ -7,6 +7,7 @@ import {Gal} from './gal.js';
         - 'gal.js@83' about changing on viewport. Fix switching from mobile to desktop.
         - check: Desktop:\\gal-style.check.jpg: '.pop-cart-create' ./style.css@353 positioning; ./js/galmq.js@21,22 behavior. Dependend on parent containers' height.
     TODO: 
+        - transition issue (a bit mess): check out around index.html@22, style.css@479, js/gal.js@220;
         - transitioning leftbar: (ref: https://stackoverflow.com/questions/3331353/transitions-on-the-css-display-property)
             './style.css@490' 'index.html@22' 'js/gal.js@76'
         - check style.css@582:'overflow-y' to accomplish design on 375px instead 700px :)
